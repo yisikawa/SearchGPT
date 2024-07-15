031be3f4bac6960e49408faca21f4d77348441f1
yisikawa <yisikawa@ma.medias.ne.jp>
2024-07-15 10:26:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,38 @@
 import streamlit as st
+from openai import OpenAI
+from langchain_openai import ChatOpenAI
+#from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+    st.title("🎈 langchain-streamlit-app")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    prompt = st.chat_input("What is up")
+    if prompt:
+
+        st.session_state.messages.append({"role": "user","content":prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            chat_model = ChatOpenAI(
+                model = "gpt-3.5-turbo",
+            )
+            messages = [HumanMessage(content=prompt)]
+            respose = chat_model.invoke(messages)
+            st.markdown(respose.content)
+        st.session_state.messages.append({"role": "assistant","content":respose.content})
